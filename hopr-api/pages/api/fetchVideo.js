0bcf74d55ced79a69b8cc59ae07f99b77ad4f1c4
jjperezaguinaga <me@jjperezaguinaga.com>
2021-02-23 20:52:10 +0000

    Adding fetch video timezone

---
+++ hopr-api/pages/api/fetchVideo.js
@@ -0,0 +1,25 @@
+import dayjs from 'dayjs'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+
+dayjs.extend(isSameOrAfter)
+
+import { citiesMap } from '../constants';
+
+export default async function handler(req, res) {
+  const {
+    query: { timestamp },
+  } = req;
+
+  const now = (timestamp ? new Date(timestamp) : new Date()).getTime()
+
+  const getCurrentCityFromTimestamp = (dateTimestamp) => {
+    const citiesAfter = citiesMap.filter( city => dayjs(dateTimestamp).isSameOrAfter(city.date) )
+    return citiesAfter.length > 0 ? citiesAfter.pop() : citiesMap[0]
+  }
+
+  const cityObject = getCurrentCityFromTimestamp(now)
+
+  const city = cityObject.env
+  const url = process.env[`VIMEO_URL_${city}`]
+  res.status(200).json({ url, city, now })
+}
