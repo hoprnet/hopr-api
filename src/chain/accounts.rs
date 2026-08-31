8a39ec31a75a7e352d7c4718f64b4ecaf4825e69
NumberFour8 <lukas.pohanka@inina.net>
2026-08-31 11:45:20 +0200

    chore: deprecate `withdraw_from_signer` in favor of `withdraw` with `BasicPayloadGenerator`
    Deprecate API function which will be removed in the next major release (5.0.0).

--- src/chain/accounts.rs
+++ src/chain/accounts.rs
@@ -72,6 +72,9 @@ pub trait ChainWriteAccountOperations {
     ) -> Result<BoxFuture<'life0, Result<ChainReceipt, Self::Error>>, Self::Error>;
 
     /// Withdraws native or token currency from the `signer` to `recipient` in a single transaction.
+    ///
+    /// **Deprecated:** Use `withdraw` with `BasicPayloadGenerator` instead. Will be removed in 5.0.0
+    #[deprecated(since = "4.2.0", note = "Use `withdraw` with `BasicPayloadGenerator` instead")]
     async fn withdraw_from_signer<C: Currency + Send>(
         &self,
         signer: &ChainKeypair,
